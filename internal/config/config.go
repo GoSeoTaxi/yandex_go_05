51b0bff6b0d2353c51f074761e48b889f6b847d3
GoSeoTaxi <95137583+GoSeoTaxi@users.noreply.github.com>
2022-04-26 00:06:02 +0300

    increment 02 v4

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,5 @@
 package config
 
-const Server_host = "http://localhost"
+const ServerHost = "http://localhost"
 const Port = "8080"
 const ConstGetEndPoint = "id"
--- internal/handlers/utils.go
+++ internal/handlers/utils.go
@@ -3,5 +3,5 @@ package handlers
 import "github.com/GoSeoTaxi/yandex_go_05/internal/config"
 
 func MakeString(idItem string) string {
-	return config.Server_host + ":" + config.Port + "/?" + config.ConstGetEndPoint + "=" + idItem
+	return config.ServerHost + ":" + config.Port + "/?" + config.ConstGetEndPoint + "=" + idItem
 }
