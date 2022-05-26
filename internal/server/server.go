21381526d2d0dc6729b1ff21bbcd3b0f88fdaf5a
GoSeoTaxi <95137583+GoSeoTaxi@users.noreply.github.com>
2022-05-27 00:55:20 +0300

    Increment 09 v5

--- internal/server/server.go
+++ internal/server/server.go
@@ -29,11 +29,11 @@ func MainServer() {
 
 	r.Use(middleware.Compress(1, "gzip"))
 
-	r.Get(config.PathURLConf, handlers.MainHandlFuncGet)
-	r.With(handlers.Ungzip).Post(config.PathURLConf, handlers.MainHandlFuncPost)
+	r.With(handlers.SetCookies).Get(config.PathURLConf, handlers.MainHandlFuncGet)
+	r.With(handlers.SetCookies).With(handlers.Ungzip).Post(config.PathURLConf, handlers.MainHandlFuncPost)
 	//	r.Post(config.PathURLConf, handlers.MainHandlFuncPost)
-	r.With(handlers.Ungzip).Post("/api/shorten", handlers.APIJSON)
-	r.With(handlers.SetCookies).Get("/api/user/urls", handlers.GetAPIJSONLogin)
+	r.With(handlers.SetCookies).With(handlers.Ungzip).Post("/api/shorten", handlers.APIJSON)
+	r.With(handlers.SetCookies).With(handlers.Ungzip).Get("/api/user/urls", handlers.GetAPIJSONLogin)
 	http.ListenAndServe(":"+config.Port, r)
 
 }
